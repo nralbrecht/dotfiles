4e8f835c6b5006227fb958ef1fb5ca006a17fc29
nralbrecht <nralbrecht@web.de>
2019-06-25 01:17:50 +0200

    use hardware accelerated compositing

--- .screenlayout/default.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-# gui tool: arandr
-xrandr --output DVI-D-0 --mode 1920x1080 --pos 0x0 --rotate left --output HDMI-0 --off --output DVI-I-1 --primary --mode 1920x1080 --pos 1080x540 --rotate normal --output DVI-I-0 --off --output DP-1 --off --output DP-0 --off
