8a18f6957bae00f86b8e8a451156c78b3896d73b
nralbrecht <nralbrecht@web.de>
2019-07-02 17:43:14 +0200

    Load zsh plugins in turbo mode

--- .config/zsh/zplugin.zsh
+++ .config/zsh/zplugin.zsh
@@ -1,28 +1,41 @@
 ### ZPLUGIN
+# see: https://github.com/zdharma/zplugin
 
 source $HOME/.zplugin/bin/zplugin.zsh
 autoload -Uz _zplugin
 (( ${+_comps} )) && _comps[zplugin]=_zplugin
 
-autoload compinit
-compinit
-
 # Theme
+zplugin ice wait'!0' pick''
 zplugin load nralbrecht/gimezsh
 
 # Plugins
+zplugin ice wait'1' lucid
 zplugin load hcgraf/zsh-sudo
+
+zplugin ice wait'1' lucid
 zplugin load zdharma/history-search-multi-word
+
+zplugin ice wait'0' lucid
 zplugin snippet OMZ::plugins/extract/extract.plugin.zsh
+
+zplugin ice wait'0' lucid
 zplugin snippet OMZ::plugins/command-not-found/command-not-found.plugin.zsh
 
+zplugin ice wait'0' lucid
 zplugin load rupa/z
-zplugin load zsh-users/zsh-completions
-zplugin load leophys/zsh-plugin-fzf-finder
+
+zplugin ice wait'0' lucid
 zplugin load changyuheng/zsh-interactive-cd
 
-zplugin load zsh-users/zsh-autosuggestions
-bindkey '^ ' autosuggest-accept
-
-zplugin load zdharma/fast-syntax-highlighting
+zplugin ice wait'0' lucid
 zplugin snippet OMZ::plugins/colored-man-pages/colored-man-pages.plugin.zsh
+
+zplugin ice wait'0' lucid
+zplugin load zsh-users/zsh-completions
+
+zplugin ice wait'0' lucid atload"bindkey '^ ' autosuggest-accept"
+zplugin load zsh-users/zsh-autosuggestions
+
+zplugin ice wait'0' lucid atinit"zpcompinit; zpcdreplay"
+zplugin load zdharma/fast-syntax-highlighting
